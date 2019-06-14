5590b5fb1a4c72162a149f263fd8f941c1bf04d6
shanku007 <jhashankar0077@gmail.com>
2019-06-15 00:59:15 +0530

    font-awesome configuratio

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import './stylesheets/index.scss';
 import './scripts/index.js';
 
 
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid' // https://fontawesome.com/icons?d=gallery&s=solid&m=free
+import '@fortawesome/fontawesome-free/js/regular' // https://fontawesome.com/icons?d=gallery&s=regular&m=free
+import '@fortawesome/fontawesome-free/js/brands' // https://fontawesome.com/icons?d=gallery&s=brands&m=free
